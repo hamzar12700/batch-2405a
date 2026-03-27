e7dd76dadfca4718c8196c663bd96567f7a1edb4
Hamza Raza <hr124055@gmail.com>
2026-03-27 15:46:24 +0500

    add js files

---
+++ js/class 2/app.js
@@ -0,0 +1,47 @@
+//prompt
+
+
+// var apkaNaam = prompt("apka naam kia hai ???????") // 2
+// console.log(apkaNaam);
+// document.writeln(apkaNaam)
+
+
+//  ()  ----> functions 
+
+
+
+
+// var rohan = "rohan","waleed","ali"
+
+
+var rest = ["biryani","mandi",15,undefined, null , false];
+
+
+// console.log(rest[apkaNaam]); // [2]  --- 15
+
+
+
+
+var a = 5;
+var b = 10;
+
+
+
+var result = b % a;
+
+
+console.log(result);
+
+
+
+// console.log(rest)
+
+// rest.push("halwa pori")
+// rest.pop()
+// rest.push("nihari")
+
+
+// console.log(rest); // halwa pori
+
+
+// console.log(rest.length)
