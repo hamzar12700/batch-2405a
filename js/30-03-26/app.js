26e0f3ab5ca3f484c07c30f51e72b291f69feeb0
user.email : "hamza2203c@aptechgdn.net <hamza2203c@aptechgdn.net>
2026-03-30 11:45:17 +0500

    adding functions folder

---
+++ js/30-03-26/app.js
@@ -0,0 +1,75 @@
+// function chaiBanaDo() {
+//     console.log("pani ko boil kiya jaye ga"),
+//         console.log("patti dali jaye gyi")
+//     console.log("phir cheeni dalni pare gyi"),
+//         console.log("dhood ko dala jaye ga"),
+//         console.log("chai hamari tyar")
+// }
+
+
+// var aptech = "aptech site"
+
+// function aptech(name){ // parameter
+
+//     console.log("My Name is", name); // -----> output ??
+    
+// }
+
+// console.log(name);
+
+// aptech("hamza") // arguments 
+// aptech("rohan")
+// aptech("ali Qasim")
+
+
+
+// chaiBanaDo()
+// chaiBanaDo()
+
+
+
+
+
+// function foods(items){
+//     console.log(items[2]);
+//     console.log(items[3]);
+    
+// }
+
+
+
+// let rest = ['biryani','mandi','haleem','halwa pori'];
+
+
+// foods(rest)
+
+
+
+
+function sum (a,b,c){
+   return a,b,c
+    
+}
+
+let num1 = sum(12,20,30)
+
+console.log(num1);
+
+
+
+
+// return keyword in javascript 
+
+
+// function intro(name) {
+//     console.log(name);
+//     return;
+//     console.log("learning functions");
+    
+// }
+
+// intro("rohan")
+
+
+
+
