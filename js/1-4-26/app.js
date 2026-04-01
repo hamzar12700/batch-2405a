229207b885b9fa4c6b00b575f4d6ee7fc6a5b5d0
user.email : "hamza2203c@aptechgdn.net <hamza2203c@aptechgdn.net>
2026-04-01 10:42:40 +0500

    adding objects in detail

---
+++ js/1-4-26/app.js
@@ -0,0 +1,52 @@
+const std1 = {
+	fullName : "rohan sheikh",
+	fatherName : "Shakir",
+	age : 18,
+	campus : "aptech site center",
+	skills : ['avoid coding','snapchat','instagram','class attend'],
+	isCousreCompleted : false,  /// ========> boolean,
+    intro : function (){
+        console.log(`my name is ${this.fullName} `);
+        
+    }
+}
+
+// de-structure in object
+
+// var fullName = "waleed"
+
+const {fullName , fatherName , age , campus} = std1
+
+console.log(meraPoraName);
+
+
+
+// console.log(std1.fullName);
+// console.log(std1.fatherName );
+// console.log(std1.age);
+// console.log(std1.campus);
+// console.log(std1.skills);
+
+
+
+
+
+// console.log(std1.intro());  // ====> method 
+
+
+
+
+// const rohan =  ['avoid coding','snapchat','instagram','class attend']
+
+
+// document.writeln(rohan);
+
+
+// console.log(std1);
+
+// document.writeln(std1.fullName)
+
+// console.log(std1.fullName);
+// console.log(std1.age);
+// console.log(std1.skills[2]);
+
